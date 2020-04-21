66dfb2e09ad8aebe749c3ac66455af4fa20e6afd
rldor <64091623+rldor@users.noreply.github.com>
2020-04-22 00:34:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,15 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 import random
-from rand import cards
+from rand import *
 
 @app.route('/')
 def hello():
-
     return render_template("index.html")#,i=f'{words[0]} ({words[1][1:]})')#"s.html")
 
 
 @app.route('/bio')
 def biology():
-    from rand import s_bio, m
     s1=cards(s_bio)
     words=random.choice(s1).split(':')
     current_mode=random.choice(m)
@@ -22,7 +20,6 @@ def biology():
 
 @app.route('/bot')
 def botany():
-    from rand import s_bot, m
     s1 = cards(s_bot)
     words = random.choice(s1).split(':')
     current_mode = random.choice(m)
@@ -31,7 +28,6 @@ def botany():
 
 @app.route('/bh')
 def biochem():
-    from rand import s_bh, m
     s1 = cards(s_bh)
     words = random.choice(s1).split(':')
     current_mode = random.choice(m)
@@ -40,7 +36,6 @@ def biochem():
 
 @app.route('/chem')
 def chemistry():
-    from rand import s_chem_easy, s_chem_hard, m
     s1 = cards(s_chem_easy)
     s2 = cards(s_chem_hard)
     current_mode = random.choice(m)
